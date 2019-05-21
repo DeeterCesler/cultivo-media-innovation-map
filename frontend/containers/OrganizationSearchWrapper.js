0469bb378aadcfa33bf4b3ec075d69fe6d76c127
Henry Kaufman <hkaufman19@gmail.com>
2019-05-21 13:56:14 -0600

    Fix: Issue w/ overflow scroll on organization search wrapper

--- frontend/containers/OrganizationSearchWrapper.js
+++ frontend/containers/OrganizationSearchWrapper.js
@@ -14,6 +14,7 @@ import OrganizationCategoryShape from '../shapes/OrganizationCategory';
 const StyledSearchWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  height: 100%;
 `;
 
 const OrganizationSearchWrapper = ({
