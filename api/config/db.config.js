c69d522bd7f2b3c7b8d6c42a2049320ee062520f
Deeter <deeter.cesler@gmail.com>
2021-01-11 22:03:09 -0700

    test to see if it at least loads without data

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-// Set the DB_NAME (the db we will connect to)
-const DB_NAME = 'cultivo-media-innovation-map';
+// // Set the DB_NAME (the db we will connect to)
+// const DB_NAME = 'cultivo-media-innovation-map';
 
-const connectionUrl = `mongodb://localhost:27017/${DB_NAME}`;
+// const connectionUrl = `mongodb://localhost:27017/${DB_NAME}`;
 
-// Configure a new connection
-const connection = mongoose.connect(
-  connectionUrl,
-  {
-    useNewUrlParser: true,
-  },
-);
+// // Configure a new connection
+// const connection = mongoose.connect(
+//   connectionUrl,
+//   {
+//     useNewUrlParser: true,
+//   },
+// );
 
-module.exports = {
-  connection,
-};
+// module.exports = {
+//   connection,
+// };
